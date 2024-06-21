382ce9cb3dc3ec61b06079e2e4b8409ddb0d06c6
umamah <umamah.chowdhury@gmail.com>
2024-06-21 17:54:52 +0100

    Returns highest and lowest numbers in a given array

---
+++ maxmin.py
@@ -0,0 +1,12 @@
+def max_min(array):
+    sorted_array = sorted(array)
+    result = []
+    min = sorted_array[0]
+    result.append(min)
+    max = sorted_array[-1]
+    result.append(max)
+    return result
+
+
+
+
